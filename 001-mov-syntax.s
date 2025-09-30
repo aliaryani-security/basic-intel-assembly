70485c785cbb0ba19ec18c2c996789c254a98d2d
Ali Aryani <lenesis@proton.me>
2025-09-30 15:34:44 +0330

    a few more movs

--- 001-mov-syntax.s
+++ 001-mov-syntax.s
@@ -1,3 +1,6 @@
 ; program to demonstrate MOV instruction
 start: ; start label declares the entry point of our code
 mov AH, 0x15 ; moving 0x15 into AH register
+mov AL, 15 ;moving 15 into AL register
+mov BH, 0b1010 ; moving ob1010 into BH
+mov BL, 0xffff ; moving 0xffff into BL
